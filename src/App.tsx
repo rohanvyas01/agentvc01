41ed60e23dd062d917481564116ef24931bee73c
rohanvyas01 <rohanjobs011@gmail.com>
2025-06-24 14:43:49 +0530

    Update Landing Page with Final Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import LandingPage from './pages/LandingPage';
-import VCDatabase from './pages/VCDatabase';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/vc-database" element={<VCDatabase />} />
         </Routes>
       </div>
     </Router>
